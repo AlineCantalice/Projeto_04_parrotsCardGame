05ffeaa2967bfdcc4e586d50f259a55ac2b1a8ea
AlineCantalice <alinedecantalice@gmail.com>
2022-04-09 10:27:29 -0300

    adiciona e mostra cartas na tela

--- script.js
+++ script.js
@@ -1,11 +1,50 @@
-function iniciarJogo() {
-    let quantidade = parseInt(prompt("Com quantos pares de cartas quer jogar? [2 - 7 pares]?")) * 2;
-
-    while ((quantidade % 2) !== 0 || quantidade < 4 || quantidade > 14) {
-        quantidade = parseInt(prompt("Com quantos pares de cartas quer jogar? [2 - 7 pares]?")) * 2;
-    }
-}
+let quantidade;
+let pares;
+let movidas;
+let acertos;
+let deck = [];
+const cartas = ["images/carta_1", "images/carta_2", "images/carta_3", "images/carta_4", "images/carta_5", "images/carta_6", "images/carta_7"];
 
 iniciarJogo();
+
+function iniciarJogo() {
+  quantidade = parseInt(prompt("Com quantas cartas você quer jogar? \n O número de cartas deve ser par e estar entre 4 e 14.")) * 2;
+
+  while ((quantidade % 2) !== 0 || quantidade < 4 || quantidade > 14) {
+    quantidade = parseInt(prompt("Com quantas cartas você quer jogar? \n O número de cartas deve ser par e estar entre 4 e 14.")) * 2;
+  }
+
+  adicionarCartas();
+  colocarCartas();
+}
+
+function adicionarCartas() {
+  let li;
+  let img;
+
+  for (let i = 1; i <= quantidade; i++) {
+    li = document.createElement("li");
+    img = document.createElement("img");
+    li.classList.add("carta");
+    li.setAttribute("data-identifier", "carta");
+    li.innerHTML = `<div class="back face">
+                      <img src="images/front.png" alt="">
+                    </div>
+                    <div class="front-face face"></div>`;
+
+    img.src = `images/carta_${i}.gif`;
+
+    li.querySelector(".front-face.face").appendChild(img);
+    deck.push(li);
+  }
+  console.log(deck)
+}
+
+function colocarCartas(){
+  let lista = document.querySelector("ul");
+  deck.forEach(carta => {
+    lista.appendChild(carta);
+  });
+}
 
 
